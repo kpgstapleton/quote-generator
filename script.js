58fc83d42d832cfb008cb84b84a7f9041f828261
Kevin Stapleton <kpgstapleton@gmail.com>
2022-03-29 19:59:49 +0100

    initial commit

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-console.log('testing');
+// Get quotes from API
+
+let apiQuotes = [];
+
+// show new quote
+function newQuote(){
+  // Pick a random quote from API Quotes
+  const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+  console.log(quote);
+} 
+
+async function getQuotes() {
+  const apiUrl = 'https://type.fit/api/quotes';
+
+  try{
+    const response = await fetch(apiUrl);
+    apiQuotes = await response.json();
+    newQuote();
+
+  } catch (error) {
+    // handle error here
+  }
+}
+
+// On Load
+getQuotes();
