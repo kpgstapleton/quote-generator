72cad30196c16e55efdc472a7e3c5c4d3b4b4ad7
Kevin Stapleton <kpgstapleton@gmail.com>
2022-04-06 09:23:44 +0100

    added spinner, buttons work

--- script.js
+++ script.js
@@ -1,15 +1,50 @@
 // Get quotes from API
 
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
+
 let apiQuotes = [];
+
+// Show loading
+function loading(){
+  loader.hidden = false;
+  quoteContainer.hidden = true;
+}
+// Hide loader
+function complete(){
+  quoteContainer.hidden = false;
+  loader.hidden = true;
+}
 
 // show new quote
 function newQuote(){
+  loading();
   // Pick a random quote from API Quotes
   const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
-  console.log(quote);
+  // Check if Author field is blank and replace it with unknown
+  if(!quote.author) {
+    authorText.textContent = 'Unknown';
+  } else {
+    authorText.textContent = quote.author;
+  }
+
+  // Check quote length and ajust styling
+  if(quote.text.length > 120) {
+    quoteText.classList.add('long-quote');
+  } else {
+    quoteText.classList.remove('long-quote');
+  }
+  // Set quote, hide loader
+  quoteText.textContent = quote.text;
+  complete();
 } 
 
 async function getQuotes() {
+  loading();
   const apiUrl = 'https://type.fit/api/quotes';
 
   try{
@@ -22,5 +57,14 @@ async function getQuotes() {
   }
 }
 
+// Tweet Quote
+function tweetQuote(){
+  const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+  window.open(twitterUrl, '_blank')
+}
+
+//Event Listeners
+newQuoteBtn.addEventListener('click', newQuote)
+twitterBtn.addEventListener('click', tweetQuote);
 // On Load
 getQuotes();
